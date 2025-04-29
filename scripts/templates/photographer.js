3a67eef9781ff6ad6a37bd7d1abcff6a5f4d2f0d
l-nikola <legay.nikola.pro@gmail.com>
2025-04-29 21:16:29 +0200

    feat : use DOMParser + add missing data

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,23 @@
 function photographerTemplate(data) {
   const { name, portrait } = data;
-
   const picture = `assets/images/Photographers ID Photos/${portrait}`;
 
   function getUserCardDOM() {
-    const article = document.createElement("article");
-    const img = document.createElement("img");
-    img.setAttribute("src", picture);
-    const h2 = document.createElement("h2");
-    h2.textContent = name;
-    article.appendChild(img);
-    article.appendChild(h2);
-    return article;
+    const parser = new DOMParser();
+
+    const article = `
+      <article>
+        <img src="${picture}" alt="${name}">
+        <h2>${name}</h2>
+        <span>${data.city}, ${data.country}</span>
+        <span>${data.tagline}</span>
+        <span>${data.price}€/jour</span>
+      </article>
+    `;
+
+    // Converti la chaîne HTML en un document DOM.
+    const doc = parser.parseFromString(article, "text/html");
+    return doc.querySelector("article");
   }
   return { name, picture, getUserCardDOM };
 }
