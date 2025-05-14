23bcc0ffa16e3c55860003ca7c105baccd6da342
l-nikola <legay.nikola.pro@gmail.com>
2025-05-14 21:06:41 +0200

    feat : lightbox style

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -18,11 +18,11 @@ export function lightbox(src, mediaTitle, mediaArray) {
               ? `<video class="lightbox__media" src="${src}" controls autoplay></video>`
               : `<img class="lightbox__media" src="${src}" alt="Lightbox media">`
           }
-          <div class="lightbox__controls">
-            <button class="lightbox__prev" aria-label="Previous media">
+          <div class="lightbox__controls lightboxChevronContainer">
+            <button class="lightbox__prev lightboxChevron" aria-label="Previous media">
               <i class="fa-solid fa-chevron-left"></i>
             </button>
-            <button class="lightbox__next" aria-label="Next media">
+            <button class="lightbox__next lightboxChevron" aria-label="Next media">
               <i class="fa-solid fa-chevron-right"></i>
             </button>
           </div>
